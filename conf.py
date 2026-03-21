a1e2ac7492c777650396833e48df20eebe39725c
WISROVI <wisrovi@debian.com>
2026-03-22 00:57:18 +0100

    Add conf.py to root for RTD compatibility

---
+++ conf.py
@@ -0,0 +1,95 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+
+project = "wpipe"
+copyright = "2024-2026, William Steve Rodriguez Villamizar"
+author = "William Steve Rodriguez Villamizar"
+version = "1.0.0"
+release = "1.0.0"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.graphviz",
+    "myst_parser",
+    "sphinx_copybutton",
+    "sphinx_design",
+]
+
+templates_path = ["docs/source/_templates"]
+exclude_patterns = []
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["docs/source/_static"]
+html_css_files = ["docs/source/_static/css/custom.css"]
+
+html_theme_options = {
+    "logo_only": False,
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": False,
+    "style_nav_header_force": True,
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
+}
+
+html_scaled_image_link = False
+html_permalinks_icon = "¶"
+
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
+}
+
+autodoc_typehints = "description"
+autodoc_member_order = "bysource"
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_type_aliases = None
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "requests": ("https://requests.readthedocs.io/en/latest/", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
+}
+
+graphviz_output_format = "svg"
+
+copybutton_prompt_text = r">>> |\.\.\. "
+copybutton_prompt_is_regexp = True
+copybutton_copy_empty_lines = False
+copybutton_here_doc_delimiter = "<<<<"
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "tasklist",
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+master_doc = "index"
+docs_path = "docs/source"
