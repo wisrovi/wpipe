3823f78a69c771fbeb12fb3a04625227d4d4a250
WISROVI <wisrovi@debian.com>
2026-03-31 05:19:02 +0200

    Export Phase 1 core features in main module

--- wpipe/__init__.py
+++ wpipe/__init__.py
@@ -3,20 +3,30 @@ wpipe - A Python library for creating and executing pipelines with task orchestr
 """
 
 from .api_client import APIClient
+from .checkpoint import CheckpointManager
 from .dashboard import start_dashboard
+from .export import PipelineExporter
 from .log import new_logger
-from .pipe import Condition, Pipeline
+from .pipe import Condition, Pipeline, PipelineAsync
 from .ram import memory
 from .sqlite import Wsqlite
+from .timeout import timeout_sync, timeout_async, TaskTimer, TimeoutError
 from .tracking import PipelineTracker
 
 __all__ = [
     "APIClient",
+    "CheckpointManager",
     "Condition",
     "Pipeline",
+    "PipelineAsync",
+    "PipelineExporter",
+    "TaskTimer",
+    "TimeoutError",
     "Wsqlite",
     "memory",
     "new_logger",
     "start_dashboard",
     "PipelineTracker",
+    "timeout_async",
+    "timeout_sync",
 ]
