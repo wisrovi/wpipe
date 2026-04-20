07be22bb300be30499ed44356feb2d2de4d33149
williamrv-edt <138199553+williamrv-edt@users.noreply.github.com>
2026-04-20 11:40:27 +0200

    docs: Update version to 1.6.10 in Sphinx conf and index.html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,8 @@ project = "wpipe"
 copyright = f"2024-{datetime.now().year}, William Steve Rodriguez Villamizar"
 author = "William Steve Rodriguez Villamizar"
 author_url = "https://github.com/wisrovi"
-version = "1.6.4"
-release = "1.6.4"
+version = "1.6.10"
+release = "1.6.10"
 
 extensions = [
     "sphinx.ext.autodoc",
