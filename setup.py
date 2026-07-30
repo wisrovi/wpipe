d1066aba41cc204bd0f9960fd2a3a9055c69a0f5
williamrv-edt <138199553+williamrv-edt@users.noreply.github.com>
2026-07-30 15:06:34 +0200

    [FEATURE] Bump version to 2.4.3 in setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def run_setup() -> None:
 
     setup(
         name="wpipe",
-        version="2.4.2",
+        version="2.4.3",
         description="Library for creating pipelines connected to an API",
         author="William Steve Rodriguez Villamizar",
         author_email="wisrovi.rodriguez@gmail.com",
